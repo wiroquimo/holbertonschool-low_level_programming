867e3514505bd04c1697db7fa18b2b32d94a0221
wiroquimo <rolandoquiroz@outlook.com>
2024-03-17 21:55:51 +0000

    Add Tasks 0 and 1

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * init_dog - Function that initialize a variable of type struct dog
+ * @d: Name of the variable
+ * @name: Parameter name
+ * @age: Parameter age
+ * @owner: Parameter owner
+ *
+ * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		return;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
---
+++ structures_typedef/dog.h
@@ -0,0 +1,25 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+ * struct dog - Defines a new structure dog.
+ * @name: char *name parameter for the name.
+ * @age: float *age parameter for the age.
+ * @owner: char *owner parameter for the owner.
+ *
+ * Description: Longer description
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+int _putchar(char c);
+
+#endif
