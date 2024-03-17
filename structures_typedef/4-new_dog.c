605dfe11b9638ed69fa214adbd9066920fe8167d
wiroquimo <rolandoquiroz@outlook.com>
2024-03-17 22:30:30 +0000

    Add task 4

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,67 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * _strlen - function that returns the length of a string.
+ * @s: char *s String to be processed
+ * Return: the length of a string
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	len = 0;
+	while (s[len] != '\0')
+	{
+		++len;
+	}
+	return (len);
+}
+/**
+ * new_dog - Function that creates a new type dog struct.
+ * @name: Parameter name.
+ * @age: Parameter age.
+ * @owner:Paramter owner.
+ * Return: A type struct dog.
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *puppie;
+	int name_length = 0, owner_length = 0, i;
+
+	name_length = _strlen(name);
+	owner_length = _strlen(owner);
+
+	if (name == NULL || owner == NULL)
+		return (NULL);
+
+	puppie = malloc(sizeof(dog_t));
+	if (puppie == NULL)
+		return (NULL);
+
+	puppie->name = malloc(name_length + 1);
+	if (puppie->name == NULL)
+	{
+		free(puppie);
+		return (NULL);
+	}
+
+	puppie->owner = malloc(owner_length + 1);
+	if (puppie->owner == NULL)
+	{
+		free(puppie->name);
+		free(puppie);
+		return (NULL);
+	}
+
+	for (i = 0; i < (name_length + 1); i++)
+		puppie->name[i] = name[i];
+
+	puppie->age = age;
+
+	for (i = 0; i < (owner_length + 1); i++)
+		puppie->owner[i] = owner[i];
+
+	return (puppie);
+}
