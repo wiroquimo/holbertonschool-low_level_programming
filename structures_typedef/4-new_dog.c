2369d63bb88b6cc4e21bb7f467b03a96dfd3ce9d
wiroquimo <rolandoquiroz@outlook.com>
2024-03-18 00:05:07 +0000

    Add task 4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -9,12 +9,14 @@
  */
 int _strlen(char *s)
 {
-	int length = 0;
+	int len;
 
-	while (s[length])
-		length++;
-
-	return (length);
+	len = 0;
+	while (s[len] != '\0')
+	{
+		++len;
+	}
+	return (len);
 }
 
 /**
