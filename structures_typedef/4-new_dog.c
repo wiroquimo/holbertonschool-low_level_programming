f1ef43bdf5766c63d7a1b42df28751d5c06f5fd8
wiroquimo <rolandoquiroz@outlook.com>
2024-03-18 20:05:28 -0500

    Add task 4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -27,40 +27,40 @@ int _strlen(char *s)
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    dog_t *puppie = NULL;
-    int i;
+	dog_t *puppie = NULL;
+	int i;
 
-    if (name == NULL || owner == NULL)
-        return (NULL);
+	if (name == NULL || owner == NULL)
+		return (NULL);
 
-    puppie = malloc(sizeof(dog_t));
-    if (puppie == NULL)
-    return (NULL);
+	puppie = malloc(sizeof(dog_t));
+	if (puppie == NULL)
+		return (NULL);
 
-    puppie->name = malloc(_strlen(name) + 1);
-    if (puppie->name == NULL)
-    {
-        free(puppie);
-        return (NULL);
-    }
+	puppie->name = malloc(_strlen(name) + 1);
+	if (puppie->name == NULL)
+	{
+		free(puppie);
+		return (NULL);
+	}
 
-    puppie->owner = malloc(_strlen(owner) + 1);
-    if (puppie->owner == NULL)
-    {
-        free(puppie->name);
-        free(puppie);
-        return (NULL);
-    }
+	puppie->owner = malloc(_strlen(owner) + 1);
+	if (puppie->owner == NULL)
+	{
+		free(puppie->name);
+		free(puppie);
+		return (NULL);
+	}
 
-    for(i = 0; name[i]; i++)
-        puppie->name[i] = name[i];
-    puppie->name[i] = '\0';
+	for (i = 0; name[i]; i++)
+		puppie->name[i] = name[i];
+	puppie->name[i] = '\0';
 
-    puppie->age = age;
+	puppie->age = age;
 
-    for(i = 0; owner[i]; i++)
-        puppie->owner[i] = owner[i];
-    puppie->owner[i] = '\0';
+	for (i = 0; owner[i]; i++)
+		puppie->owner[i] = owner[i];
+	puppie->owner[i] = '\0';
 
-    return (puppie);
+	return (puppie);
 }
