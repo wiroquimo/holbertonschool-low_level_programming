dbdce554b4cbf5b36cc2ae35239d8d0c7595e1c7
rolandoquiroz <rolandoquiroz@gmail.com>
2024-08-07 11:16:45 -0500

    Add task 3: 3-cp.c

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -4,28 +4,6 @@
 #include <fcntl.h>
 
 /**
- * close_file_description - funcition that close a file description and
- * exit with error if it's the case
- * @fd_from: file description of origin
- * @fd_to: file description ot destination
-*/
-
-void close_file_description(int fd_from, int fd_to)
-{
-	if (close(fd_from) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
-		exit(100);
-	}
-
-	if (close(fd_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
-		exit(100);
-	}
-}
-
-/**
  * main - copies the content of one file to another
  * @argc: argument count
  * @argv: argument vector
@@ -34,44 +12,41 @@ void close_file_description(int fd_from, int fd_to)
  */
 int main(int argc, char *argv[])
 {
-	int fd_from, fd_to; /* file descriptors */
-	int bytes_read, bytes_written;
-	char buffer[1024];
+	int ffrom, fto, rd, clf, clt;
+	char buff[BUFSIZ];
 
-	if (argc != 3) /* Check if the number of arguments is correct */
+	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	fd_from = open(argv[1], O_RDONLY); /* Open the source file */
-	if (fd_from == -1)
+	ffrom = open(argv[1], O_RDONLY);
+	if (ffrom  == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-
-	/*Open or create the destination file, with appropriate permission */
-	fd_to = open(argv[2], O_WRONLY | O_CREAT |
-	O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-	if (fd_to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
-	while ((bytes_read = read(fd_from, buffer, 1024)) > 0) /* Copy content */
-	{
-		bytes_written = write(fd_to, buffer, bytes_read);
-		if (bytes_written != bytes_read || fd_to == -1)
+	fto = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	while ((rd = read(ffrom, buff, BUFSIZ)) > 0)
+		if (fto == -1 || (write(fto, buff, rd) != rd))
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
-	}
-	if (bytes_read == -1)
+	if (rd == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	close_file_description(fd_from, fd_to); /*close the file descriptor*/
+	clf = close(ffrom);
+	clt = close(fto);
+	if (clf == -1 || clt == -1)
+	{
+		if (clf == -1)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", ffrom);
+		else if (clt == -1)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fto);
+		exit(100);
+	}
 	return (0);
 }
