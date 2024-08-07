12853d3226fb0e1c2968802a0356d45a3a89b3d5
rolandoquiroz <rolandoquiroz@gmail.com>
2024-08-07 11:34:15 -0500

    Add task 3: 3-cp.c

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -31,7 +31,7 @@ int main(int argc, char *argv[])
 		if (fto == -1 || (write(fto, buff, rd) != rd))
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			exit(99);
+			exit(100);
 		}
 	if (rd == -1)
 	{
