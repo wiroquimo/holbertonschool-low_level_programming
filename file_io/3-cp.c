9405ca2abde477508750bde39237757c924387a5
rolandoquiroz <rolandoquiroz@gmail.com>
2024-08-07 11:09:19 -0500

    Add task 3: 3-cp.c

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -10,45 +10,43 @@
  *
  * Return: 0 if success
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	int fd, fd2, filecheck;
-	char buffer[1024];
+	int ffrom, fto, rd, clf, clt;
+	char buff[BUFSIZ];
 
 	if (argc != 3)
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
-	fd = open(argv[1], O_RDONLY);
-	if (fd == -1)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	ffrom = open(argv[1], O_RDONLY);
+	if (ffrom  == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	fd2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (fd2 == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
-	while ((filecheck = read(fd, buffer, 1024)) > 0)
-	{
-		if (filecheck == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-			exit(98);
-		}
-		filecheck = write(fd2, buffer, filecheck);
-		if (filecheck == -1)
+	fto = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	while ((rd = read(ffrom, buff, BUFSIZ)) > 0)
+		if (fto == -1 || (write(fto, buff, rd) != rd))
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
-	}
-	if (filecheck == -1)
+	if (rd == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	if (close(fd) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd), exit(100);
-	if (close(fd2) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2), exit(100);
-
+	clf = close(ffrom);
+	clt = close(fto);
+	if (clf == -1 || clt == -1)
+	{
+		if (clf == -1)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", ffrom);
+		else if (clt == -1)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fto);
+		exit(100);
+	}
 	return (0);
 }
