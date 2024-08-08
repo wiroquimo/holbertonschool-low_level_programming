6e6daca4a17b09078c4be0aba0a6fb5d7f659035
rolandoquiroz <rolandoquiroz@gmail.com>
2024-08-07 22:09:13 -0500

    Add task 3: cp

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -4,62 +4,81 @@
 #include <fcntl.h>
 
 /**
- * main - copies the content of one file to another
- * @argc: argument count
- * @argv: argument vector
- *
- * Return: 0 if success
+ * exit_97 - exit status 97
  */
-int main(int argc, char *argv[])
+void exit_97(void)
 {
-    int ffrom, fto, rd;
-    ssize_t clf, clt;
-    char buff[BUFSIZ];
+	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+	exit(97);
+}
 
-    if (argc != 3)
-    {
-        dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-        exit(97);
-    }
-    ffrom = open(argv[1], O_RDONLY);
-    if (ffrom == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-        exit(98);
-    }
-    fto = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-    if (fto == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-        close(ffrom);
-        exit(99);
-    }
-    while ((rd = read(ffrom, buff, BUFSIZ)) > 0)
-    {
-        if (write(fto, buff, rd) != rd)
-        {
-            dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-            close(ffrom);
-            close(fto);
-            exit(99);
-        }
-    }
-    if (rd == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-        close(ffrom);
-        close(fto);
-        exit(98);
-    }
-    clf = close(ffrom);
-    clt = close(fto);
-    if (clf == -1 || clt == -1)
-    {
-        if (clf == -1)
-            dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", ffrom);
-        if (clt == -1)
-            dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fto);
-        exit(100);
-    }
-    return (0);
+/**
+ * exit_98 - exit status 98
+ * @str: name of file
+ */
+void exit_98(char *str)
+{
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", str);
+	exit(98);
+}
+
+/**
+ * exit_99 - exit status 99
+ * @str: name of file
+ */
+void exit_99(char *str)
+{
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", str);
+	exit(99);
+}
+
+/**
+ * exit_100 - exit status 100
+ * @fd: name of file
+ */
+void exit_100(int fd)
+{
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+	exit(100);
+}
+
+/**
+ * main - copies the contents of one file to another
+ * @argc: integer number of arguments
+ * @argv: pointer to string arguments
+ *
+ * Return: 0 on success, or one of 97, 98, 99, 100 on failure
+ */
+int main(int argc, char **argv)
+{
+	char buffer[1024];
+	ssize_t fd_src = 0, fd_dest = 0, bytes_read = 0, bytes_written = 0;
+
+	if (argc != 3)
+		exit_97();
+
+	fd_src = open(argv[1], O_RDONLY);
+	bytes_read = read(fd_src, buffer, 1024);
+
+	fd_dest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+
+	do {
+		if ((fd_src == -1) || (bytes_read == -1))
+			exit_98(argv[1]);
+
+		bytes_written = write(fd_dest, buffer, bytes_read);
+		if ((fd_dest == -1) || (bytes_written < bytes_read))
+			exit_99(argv[2]);
+
+		bytes_read = read(fd_src, buffer, 1024);
+		fd_dest = open(argv[2], O_WRONLY | O_APPEND);
+	} while (bytes_read > 0);
+
+	if (close(fd_src) == -1)
+		exit_100(fd_src);
+
+	if (close(fd_dest) == -1)
+		exit_100(fd_dest);
+
+	return (0);
 }
